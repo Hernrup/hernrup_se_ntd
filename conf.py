2b335f9cd8059702f55c236417a79518958e0d61
Mikael Hernrup <mikael@hernrup.se>
2015-09-15 23:12:30 +0200

    modified config

--- conf.py
+++ conf.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
-from ntd import conf_debug
+import os
+import sys
 
+sys.path.append(os.curdir)
 
+AUTHOR = 'Mikael Hernrup'
+SITENAME = 'NowThinkDammit'
 SITEURL = 'http://hernrup.github.io/hernrup_se_ntd/'
-RELATIVE_URLS = False
 
+PATH = 'content'
+OUTPUT_PATH = '/output/'
+DELETE_OUTPUT_DIRECTORY = True
+
+TIMEZONE = 'Europe/Stockholm'
+
+DEFAULT_LANG = 'en'
+
+LOAD_CONTENT_CACHE = False
+
+# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
-DELETE_OUTPUT_DIRECTORY = True
-OUTPUT_PATH = '/output/'
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = False
+RELATIVE_URLS = True
---
+++ conf_dev.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from ntd.conf import *
+
+SITEURL = ''
+LOAD_CONTENT_CACHE = False
+RELATIVE_URLS = True
+DELETE_OUTPUT_DIRECTORY = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
